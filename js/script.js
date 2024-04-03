121320e238375a5ead8f3a57ae113f109cdabef8
georgegregjr <gwest1@hawk.iit.edu>
2024-04-02 20:26:28 -0500

    Should be all changes, test time

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
   var alertbut = document.getElementById('entrybutton');
- 
-
-
-
-
-
-
+  
+  function showText(){
+    var intext = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = intext.value;
+    alert("George West: "+ intext.value);
+  }
+  
+  alertbut.addEventListener('click', showText);
+  }
 
 window.addEventListener('load', init);
